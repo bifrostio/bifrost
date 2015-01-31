6320c4da6232fcb8291484f50e9308c2b0743c52
chialin <chialin.shr@gmail.com>
2015-01-31 22:10:26 +0800

    fix init error, add there new field of project

--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -9,8 +9,8 @@ module.exports = function(app) {
   });
 
   app.models.Project.create([
-    {"name": "test1 name","description": "test1","zipcode": "112","city": "Taipei","district": "tet","detailAddress": "Taipei","ownerId": 1,"userId": 1 },
-    {"name": "test2 name","description": "test2","zipcode": "112","city": "Taipei","district": "tet","detailAddress": "Taipei","ownerId": 2,"userId": 2 }
+    {"name": "test1 name","description": "test1","zipcode": "112","city": "Taipei","district": "tet","detailAddress": "Taipei","contactName": "chialin","contactEmail": "chialin.shr@gmail.com","contactPhone":"0911111111","ownerId": 1,"userId": 1 },
+    {"name": "test2 name","description": "test2","zipcode": "112","city": "Taipei","district": "tet","detailAddress": "Taipei","contactName": "yuren","contactEmail": "yurenju@gmail.com","contactPhone":"0911111111","ownerId": 2,"userId": 2 }
   ], function(err, Projects) {
     if (err) throw err;
  
