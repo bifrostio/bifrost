0fa462255b3816489a5404f49c03b154375ab34a
chialin <chialin.shr@gmail.com>
2015-03-04 02:20:51 +0800

    add alert CAP info on map

--- client/scripts/controllers/map.js
+++ client/scripts/controllers/map.js
@@ -5,14 +5,47 @@
 (function() {
   var app = angular.module('bifrost');
 
-  app.controller('MapController', function($scope, $rootScope, $timeout,
-                                           addressResolver, leafletData) {
+  app.controller('MapController', function($scope, $rootScope, $timeout, $http,
+                                           addressResolver, leafletData,
+                                           feedService, Alert, async) {
     $scope.defaults = {
       tileLayer: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       zoomControlPosition: 'bottomright',
       detectRetina: true
     };
     $scope.markers = {};
+    var entries, map;
+
+    async.series([
+      function(done) {
+        feedService.parseFeed('https://alerts.ncdr.nat.gov.tw/RssAtomFeed.ashx')
+        .then(function(res) {
+          entries = res.data.responseData.feed.entries;
+          done();
+        })
+      },
+      function(done) {
+        leafletData.getMap().then(function(resMap) {
+          map = resMap;
+          done();
+        });
+      }
+    ], function() {
+      angular.forEach(entries, function(f) {
+        Alert.get({url: f.link}).$promise.then(function(cap) {
+          var info = cap.alert.info[0];
+          var description = info.description[0];
+          var location = info.area[0].circle[0];
+          var circle = /(.*,.*) .*/g.exec(location)[1].split(',');
+
+          L.circle(circle, 1000, {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5
+          }).addTo(map).bindPopup(description);
+        });
+      });
+    });
 
     function setupMaxBounds(data) {
       var bounds = data.results[0].geometry.bounds ||
