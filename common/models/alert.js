959e92648d36b1fb4336ae86f57020f7f696e9f3
Yuren Ju <yurenju@gmail.com>
2015-03-04 01:19:12 +0800

    Returns object instead of string

--- common/models/alert.js
+++ common/models/alert.js
@@ -1,18 +1,25 @@
 module.exports = function(Alert) {
   var request = require('request');
+  var parseString = require('xml2js').parseString;
 
   Alert.get = function(url, cb) {
     request(url, function (error, response, body) {
       if (!error && response.statusCode === 200) {
-        return cb(null, body);
+        parseString(body, function(err, result) {
+          if (err) {
+            return cb(err);
+          }
+          cb(null, result);
+        });
+      } else {
+        cb(error);
       }
-      return cb(error);
     });
   };
 
   Alert.remoteMethod('get', {
     accepts: { arg: 'url', type: 'string' },
-    returns: { arg: 'cap', type: 'string', root: true},
+    returns: { arg: 'cap', type: 'object', root: true},
     http: {path: '/get', verb: 'get'}
   });
 };
