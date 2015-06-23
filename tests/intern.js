745f04ce596eb89f0986bcfdf6daf99694417de7
Yuren Ju <yurenju@gmail.com>
2015-06-23 23:35:37 +0800

    Enable multiple browser such as IE, Firefox and Safari

--- tests/intern.js
+++ tests/intern.js
@@ -23,13 +23,13 @@ define({
   // capabilities options specified for an environment will be copied as-is
   environments: [
     { browserName: 'chrome' },
-    /*
+
     // Uncomment to test out the following:
     { browserName: 'internet explorer', version: '10', platform: [ 'Windows 7' ] },
     { browserName: 'firefox', version: '21', platform: 'Mac 10.6' },
     { browserName: 'chrome', platform: [ 'Linux', 'Mac 10.8', 'Windows 7' ] },
     { browserName: 'safari', version: '6', platform: 'Mac 10.8' }
-    */
+
   ],
 
   tunnel: 'SauceLabsTunnel',
