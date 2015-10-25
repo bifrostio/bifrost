7155ca7a5690a576ba4e0466b9b9c116f969e68a
chialin <chialin.shih@hwtrek.com>
2015-10-25 14:55:29 +0800

    [Add] add react structure

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,19 @@
-document.write("New Bifrost");
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Header from 'components/Header';
+
+export default class Bifrost extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Bifrost />, document.getElementById('bifrost'));
