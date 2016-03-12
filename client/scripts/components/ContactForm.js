51d7fab13fd7ba6150c58a97a6c23295d88eac96
Yuren Ju <yurenju@gmail.com>
2016-03-12 16:55:12 +0800

    Added ContactForm and modal for confirmation

--- client/scripts/components/ContactForm.js
+++ client/scripts/components/ContactForm.js
@@ -2,12 +2,24 @@ import React, {Component} from 'react';
 import { Input } from 'react-bootstrap';
 
 export default class ContactForm extends Component {
+  handleChange(e) {
+    let field = e.target.id;
+    let props = {
+      name: this.props.name,
+      email: this.props.email,
+      phone: this.props.phone,
+      note: this.props.note
+    };
+    props[field] = e.target.value;
+    this.props.updateContact(props);
+  }
   render() {
     return (
       <form>
-        <Input type="text" label="姓名" />
-        <Input type="email" label="電子郵件" />
-        <Input type="text" label="電話" />
+        <Input onChange={this.handleChange.bind(this)} id="name" type="text" label="姓名" />
+        <Input onChange={this.handleChange.bind(this)} id="email" type="email" label="電子郵件" />
+        <Input onChange={this.handleChange.bind(this)} id="phone" type="text" label="電話" />
+        <Input onChange={this.handleChange.bind(this)} id="note" type="textarea" label="備註" />
       </form>
     );
   }
