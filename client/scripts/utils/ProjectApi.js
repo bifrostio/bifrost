b99bc9f0d927eae967e3da750374677c8eba8701
chialin <chialin.shih@hwtrek.com>
2016-03-13 00:19:46 +0800

    [ADD] add create project

--- client/scripts/utils/ProjectApi.js
+++ client/scripts/utils/ProjectApi.js
@@ -20,4 +20,23 @@ export default class ProjectApi {
       failCallback(e.status);
     });
   }
+
+  static createProject(body, doneCallback, failCallback) {
+    const token = sessionStorage.getItem('token');
+
+    if (!token) {
+      failCallback(401);
+      return;
+    }
+
+    const path = `/api/projects?access_token=${token}`;
+
+    $.post(path, body)
+    .done(function(result) {
+      doneCallback(result);
+    })
+    .fail(function(e) {
+      failCallback(e.status);
+    });
+  }
 }
