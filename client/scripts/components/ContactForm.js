e15e70da22bd4ecb7dbd4cc8d5776b82c14fa045
Yuren Ju <yurenju@gmail.com>
2016-03-12 22:14:35 +0800

    Added Confirmation

--- client/scripts/components/ContactForm.js
+++ client/scripts/components/ContactForm.js
@@ -5,10 +5,10 @@ export default class ContactForm extends Component {
   handleChange(e) {
     let field = e.target.id;
     let props = {
-      name: this.props.name,
-      email: this.props.email,
-      phone: this.props.phone,
-      note: this.props.note
+      name: this.props.contact.name,
+      email: this.props.contact.email,
+      phone: this.props.contact.phone,
+      note: this.props.contact.note
     };
     props[field] = e.target.value;
     this.props.updateContact(props);
@@ -16,10 +16,10 @@ export default class ContactForm extends Component {
   render() {
     return (
       <form>
-        <Input onChange={this.handleChange.bind(this)} id="name" type="text" label="姓名" />
-        <Input onChange={this.handleChange.bind(this)} id="email" type="email" label="電子郵件" />
-        <Input onChange={this.handleChange.bind(this)} id="phone" type="text" label="電話" />
-        <Input onChange={this.handleChange.bind(this)} id="note" type="textarea" label="備註" />
+        <Input value={this.props.contact.name} onChange={this.handleChange.bind(this)} id="name" type="text" label="姓名" />
+        <Input value={this.props.contact.email} onChange={this.handleChange.bind(this)} id="email" type="email" label="電子郵件" />
+        <Input value={this.props.contact.phone} onChange={this.handleChange.bind(this)} id="phone" type="text" label="電話" />
+        <Input value={this.props.contact.note} onChange={this.handleChange.bind(this)} id="note" type="textarea" label="備註" />
       </form>
     );
   }
