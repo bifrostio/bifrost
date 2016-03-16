e3fbca55eb3acf2093688bc3c22420a6fceb9392
chialin <chialin.shih@hwtrek.com>
2016-03-16 23:56:41 +0800

    [ADD] add manager station page/route

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,17 +1,15 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Station from 'components/Station';
+import StationManager from 'components/StationManager';
 import Gallery from 'components/Gallery';
 import MapView from 'components/MapView';
 import Login from 'components/Login';
 import ProjectList from 'components/ProjectList';
 import ProjectContent from 'components/ProjectContent';
-import { Router, Route, IndexRoute } from 'react-router';
-import { createHistory, useBasename } from 'history';
-
-const history = useBasename(createHistory)({
-  basename: '/'
-});
+import ProvisionRequirement from 'components/ProvisionRequirement';
+import ProvisionActivity from 'components/ProvisionActivity';
+import { Router, Route, IndexRoute, IndexRedirect, hashHistory } from 'react-router';
 
 export default class Bifrost extends Component {
   render() {
@@ -24,10 +22,15 @@ export default class Bifrost extends Component {
 }
 
 ReactDOM.render((
-  <Router>
+  <Router history={hashHistory}>
     <Route path='/' component={Bifrost}>
       <IndexRoute component={MapView}/>
       <Route path='stations/:id' component={Station} />
+      <Route path='manager/station/:id' component={StationManager} >
+        <IndexRedirect to='requirement' />
+        <Route path='requirement' component={ProvisionRequirement} />
+        <Route path='activity' component={ProvisionActivity} />
+      </Route>
       <Route path='gallery' component={Gallery} />
       <Route path='login' component={Login} />
       <Route path='projects' component={ProjectList} />
