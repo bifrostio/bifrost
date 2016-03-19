5e5776917efbc802bda8f96c9073936926d9705d
Yuren Ju <yurenju@gmail.com>
2016-03-19 23:53:41 +0800

    Added validator to contactform

--- client/scripts/components/ContactForm.js
+++ client/scripts/components/ContactForm.js
@@ -1,7 +1,24 @@
 import React, {Component} from 'react';
 import { Input } from 'react-bootstrap';
+import validator from 'validator';
 
 export default class ContactForm extends Component {
+  validationState(key) {
+    var prop = this.props.contact[key];
+    if (typeof(prop) === 'undefined') {
+      return;
+    }
+    else if (key === 'email') {
+      return validator.isEmail(prop) ? 'success' : 'error';
+    }
+    else if (key === 'phone') {
+      return validator.isNumeric(prop) ? 'success' : 'error';
+    }
+    else if (typeof(prop) === 'string') {
+      return prop.length > 0 ? 'success' : 'error';
+    }
+  }
+
   handleChange(e) {
     let field = e.target.id;
     let props = {
@@ -16,10 +33,10 @@ export default class ContactForm extends Component {
   render() {
     return (
       <form>
-        <Input value={this.props.contact.name} onChange={this.handleChange.bind(this)} id="name" type="text" label="姓名" />
-        <Input value={this.props.contact.email} onChange={this.handleChange.bind(this)} id="email" type="email" label="電子郵件" />
-        <Input value={this.props.contact.phone} onChange={this.handleChange.bind(this)} id="phone" type="text" label="電話" />
-        <Input value={this.props.contact.note} onChange={this.handleChange.bind(this)} id="note" type="textarea" label="備註" />
+        <Input bsStyle={this.validationState('name')} hasFeedback value={this.props.contact.name} onChange={this.handleChange.bind(this)} id="name" type="text" label="姓名" />
+        <Input bsStyle={this.validationState('email')} hasFeedback value={this.props.contact.email} onChange={this.handleChange.bind(this)} id="email" type="email" label="電子郵件" />
+        <Input bsStyle={this.validationState('phone')} hasFeedback value={this.props.contact.phone} onChange={this.handleChange.bind(this)} id="phone" type="text" label="電話" />
+        <Input hasFeedback value={this.props.contact.note} onChange={this.handleChange.bind(this)} id="note" type="textarea" label="備註" />
       </form>
     );
   }
